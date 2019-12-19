8c89b56a69019a31461d1463aca97d4059c9c17e
Noah Bass <noah@noahbass.com>
2019-12-18 22:03:27 -0500

    Update build runners to gulp v4 (#28)

--- bin/developBuild.js
+++ bin/developBuild.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp')
-const browserSync = require('browser-sync').create()
+const browserSync = require('browser-sync')
 const buildTemplate = require('./util/buildTemplate')
 const rename = require('./util/rename')
 const removeFolder = require('./util/removeFolder')
 
+const server = browserSync.create()
 
 // templates to watch: all templates in './templates/' (except for master.njk)
 const templates = ['./templates/*.njk']
@@ -16,18 +17,25 @@ gulp.task('clean', () => removeFolder(distFolder))
 // builds out templates to './dist/'
 // TODO: builld only the templates that have been modified
 gulp.task('build', () => {
-  gulp
+  return gulp
     .src(templates)
     .pipe(buildTemplate())
     .pipe(rename())
     .pipe(gulp.dest(distFolder))
-    .pipe(browserSync.reload({ stream: true })) // update the browser sync stream
+    .pipe(server.stream()) // update the browser sync stream
 })
 
 // serve on localhost:3000, watch for changes to templates, and update automagically with browser sync
-gulp.task('serve', ['build'], () => {
-  browserSync.init({ server: distFolder, directory: true, open: false })
-  gulp.watch(templates, ['build'])
+gulp.task('serve', (done) => {
+  server.init({ server: distFolder, directory: true, open: true })
+  done()
 })
 
-gulp.start(['clean', 'build', 'serve'])
+// watch for any changes to the templates
+gulp.task('watch', () => {
+  return gulp.watch(templates, gulp.series('build'))
+})
+
+const start = gulp.series('clean', 'build', 'serve', 'watch')
+
+start()
