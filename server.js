fd86869e9d60bd68cf92ad12a361e658551f2ed7
Dev Agrawal <devagrawal09@gmail.com>
2020-10-16 20:42:22 +0530

    added endpoints for build and send

--- server.js
+++ server.js
@@ -1,19 +1,49 @@
-const fs = require(`fs`);
-const path = require(`path`);
-const util = require(`util`);
-const express = require(`express`);
+const fs = require(`fs`)
+const path = require(`path`)
+const util = require(`util`)
+const express = require(`express`)
+const bodyParser = require(`body-parser`)
+const build = require('./lib/build')
+const send = require('./lib/send')
 
-const app = express();
+const app = express()
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 
-app.use(express.static(`dist`));
+app.use(express.static(`dist`))
+
+app.use(bodyParser.json())
 
 app.get(`/`, async (req, res) => {
-  const templates = await util.promisify(fs.readdir)(path.join(__dirname, `dist`));
-  res.render(`index`, { templates })
-});
+  try {
+    const htmlFiles = await util.promisify(fs.readdir)(path.join(__dirname, `dist`))
+    const templates = htmlFiles.map(file => ({ file, name: file.replace(`.html`, ``) }))
+    res.render(`index`, { templates })
+  } catch(err) {
+    res.status(500).send(err.message)
+  }
+})
 
-app.listen(port, () => console.log(`Listening on ${port}`));
+app.get(`/build`, async (req, res) => {
+  try {
+    const { templateName, templateData } = req.body
+    const html = await build(templateName, templateData)
+    res.json({ html })
+  } catch(err) {
+    res.status(500).send(err.message)
+  }
+})
+
+app.post(`/send`, async (req, res) => {
+  try {
+    const { mailgunApiKey, mailgunDomain, from, to, subject, html } = req.body
+    const result = await send(mailgunApiKey, mailgunDomain, from, to, subject, html)
+    res.json({ result })
+  } catch(err) {
+    res.status(500).send(err.message)
+  }
+})
+
+app.listen(port, () => console.log(`Listening on ${port}`))
