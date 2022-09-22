5dccd0def44bf3bcc76febac0d8998dc69eef14d
Bao <huynhlbg@mail.uc.edu>
2022-09-22 13:29:20 -0400

    Bump NPM versions and use 'mailgun.js' instead of deprecated 'mailgun-js'

--- bin/testEmail.js
+++ bin/testEmail.js
@@ -1,25 +1,33 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const mailgunApiKey = process.env.MAILGUN_API_KEY
-const mailgunDomain = process.env.MAILGUN_DOMAIN
-const from = 'RevolutionUC <info@revolutionuc.com>'
-const to = 'you@mail.uc.edu'
+const mailgunApiKey = process.env.MAILGUN_API_KEY;
+const mailgunDomain = process.env.MAILGUN_DOMAIN;
+const from = "RevolutionUC <info@revolutionuc.com>";
+const to = "you@mail.uc.edu";
 
-const { build, send } = require('../')
+const { build, send } = require("../");
 
-const template = 'welcome' // choose from templates in `./templates/`
+const template = "welcome"; // choose from templates in `./templates/`
 const templateData = {
-  subject: 'Welcome - Hello, world!',
-  shortDescription: 'Test message short description!'
-}
+  subject: "Welcome - Hello, world!",
+  shortDescription: "Test message short description!",
+};
 
 build(template, templateData)
-  .then(html => {
-    return send(mailgunApiKey, mailgunDomain, from, to, templateData.subject, html)
+  .then((html) => {
+    return send(
+      mailgunApiKey,
+      mailgunDomain,
+      from,
+      to,
+      templateData.subject,
+      html
+    );
   })
-  .then(() => {
-    console.log(`Sent to ${to}`)
+  .then((res) => {
+    console.log(res);
+    console.log(`Sent to ${to}`);
   })
-  .catch(error => {
-    console.error(error)
-  })
+  .catch((error) => {
+    console.error(error);
+  });
--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,18 +1,27 @@
 interface templateData {
-  firstName?: string,
-  subject?: string,
-  shortDescription?: string,
+  firstName?: string;
+  subject?: string;
+  shortDescription?: string;
   /* Used in verifyEmail template */
-  verificationUrl?: string,
+  verificationUrl?: string;
   /* Used in verfityEmail template */
-  waitlist?: boolean,
+  waitlist?: boolean;
   /* Used in confirmAttendance template */
   yesConfirmationUrl?: string;
   /* Used in confirmAttendance template */
   noConfirmationUrl?: string;
   /* Used in confirmAttendance template */
-  offWaitlist?: boolean
-
+  offWaitlist?: boolean;
 }
-export declare function build(templateName: string, templateData: templateData) : Promise<string>
-export declare function send(mailgunApiKey: string, mailgunDomain: string, from: string, to: string, subject: string, html: string): Promise<import('mailgun-js').messages.SendResponse>;
+export declare function build(
+  templateName: string,
+  templateData: templateData
+): Promise<string>;
+export declare function send(
+  mailgunApiKey: string,
+  mailgunDomain: string,
+  from: string,
+  to: string,
+  subject: string,
+  html: string
+): Promise<import("mailgunjs").messages.SendResponse>;
