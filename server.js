4dd3fd9e64472d85fff6d30bce20007eaf895ad6
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2024-02-18 14:46:34 -0500

    Update infoEmailMinors for 2024

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const sendMail = require("./lib/send");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.set("view engine", "ejs");
 
